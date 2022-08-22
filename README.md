f9b80f561c783d9dc04a31eeb57a46a83eaf7d78
Daniel Ayeni <ayenibabajidedaniel@gmail.com>
2022-08-22 18:44:42 +0100

    initial

--- README.md
+++ README.md
@@ -68,3 +68,9 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+DESIGN IDEA
+https://uidesigndaily.com/posts/figma-testimonial-day-1495
+
+
+
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Air Max 270",
+    brand: "Nike",
+    image:
+      "https://thesageconcepts.com/wp-content/uploads/2022/08/Nike-Air-270.png",
+    text:
+      "Your perfect sneakers for everyday use. Lightweight sneakers for trips or casual outings.  Drip and slay like a queen with the Air Max 270 Sneakers."
+  },
+  {
+    id: 2,
+    name: "Chuck 70 Vintage Canvas",
+    brand: "Converse",
+    image:
+      "https://thesageconcepts.com/wp-content/uploads/2022/08/Converse-chuck-70.png",
+    text:
+      "Your perfect unisex sneakers for everyday use. Lightweight sneakers for trips or casual outings. Drip is eternal so don the Chuck 7- Vintage Canvas."
+  },
+  {
+    id: 3,
+    name: "Superstar Shoes",
+    brand: "Addidas",
+    image:
+      "https://thesageconcepts.com/wp-content/uploads/2022/08/Addidas-Superstar.png",
+    text:
+      "Your perfect sneakers for everyday use. Lightweight sneakers for trips or casual outings. Drip is eternal so don the Superstar Sneakers."
+  },
+  {
+    id: 4,
+    name: "Mayze Classic Trainers",
+    brand: "Puma",
+    image:
+      "https://thesageconcepts.com/wp-content/uploads/2022/08/Puma-Mayze.png",
+    text:
+      "Your perfect sneakers for everyday use. Lightweight sneakers for trips or casual outings.  Drip and slay like a queen with the Mayze Classic Trainers. "
+  }
+];
+
+export default reviews;
